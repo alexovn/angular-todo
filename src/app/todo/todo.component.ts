002edf60f898abf8c458d9fde191670aef8b0fb6
Nikita Alexov <polarbeararmor@gmail.com>
2025-04-26 19:22:30 +0400

    feat: filter todos by its state

--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -1,4 +1,4 @@
-import { Component, signal } from '@angular/core';
+import { Component, signal, computed } from '@angular/core';
 import { TodoListComponent } from '../todo-list/todo-list.component';
 import uniqueId from '../../utils/uniqueId';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
@@ -13,6 +13,42 @@ import type { TodoItem } from '../../interfaces/todo.interface';
 export class TodoComponent {
   todo = signal('')
   todos = signal<TodoItem[]>([])
+  activeFilter = signal<'All' | 'Active' | 'Completed'>('All')
+
+  filteredTodos = computed(() => {
+    if (this.activeFilter() === 'All') {
+      return this.todos()
+    }
+    if (this.activeFilter() === 'Active') {
+      return this.todos().filter(todo => !todo.completed)
+    }
+    if (this.activeFilter() === 'Completed') {
+      return this.todos().filter(todo => todo.completed)
+    }
+    return []
+  })
+
+  filters = computed(() => ([
+    {
+      label: 'All',
+      active: this.activeFilter() === 'All',
+      click: () => this.activeFilter.set('All')
+    },
+    {
+      label: 'Active',
+      active: this.activeFilter() === 'Active',
+      click: () => this.activeFilter.set('Active')
+    },
+    {
+      label: 'Completed',
+      active: this.activeFilter() === 'Completed',
+      click: () => this.activeFilter.set('Completed')
+    }
+  ]))
+
+  filteredSentence = computed(() => {
+    return `${this.filteredTodos().length} ${this.filteredTodos().length === 1 ? 'item' : 'items'} left`
+  })
 
   addTodo() {
     const item = {
